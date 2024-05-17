7bfa67d7e22455658aca0b8dc592dc724215486d
JNapsugar <jancsurakn@kkszki.hu>
2024-05-17 11:42:29 +0200

    Szobakezelo V1

--- Kikelet_Panzió/MainWindow.xaml.cs
+++ Kikelet_Panzió/MainWindow.xaml.cs
@@ -32,9 +32,28 @@ namespace Kikelet_Panzió
             Random random = new Random();
             for (int i = 1; i <=100; i++)
             {
-                string mettol = $"{random.Next(2000, 2026)}.{random.Next(1, 13)}.{random.Next(1, 32)}";
-                szobak.Add(new Szoba(Convert.ToString(i), random.Next(1,7), random.Next(6000,12001), Convert.ToBoolean(random.Next(0,2)), "", ""));
+                string mettol = DatumCsinalo(random);
+                string meddig = DatumCsinalo(random);
+
+                while (new DateTime(int.Parse(meddig.Split('.')[0]), int.Parse(meddig.Split('.')[1]), int.Parse(meddig.Split('.')[2])) < 
+                    new DateTime(int.Parse(mettol.Split('.')[0]), int.Parse(mettol.Split('.')[1]), int.Parse(mettol.Split('.')[2])))
+                {
+                    meddig = DatumCsinalo(random);
+                }
+                szobak.Add(new Szoba(Convert.ToString(i), random.Next(1, 7), random.Next(6000, 12001), Convert.ToBoolean(random.Next(0, 2)), mettol, meddig));
             }
+        }
+
+        private static string  DatumCsinalo(Random random)
+        {
+            string honap = Convert.ToString(random.Next(1, 13));
+            string nap = Convert.ToString(random.Next(1, 32));
+            if (honap.Length <= 1)
+                honap = "0" + honap;
+            if (nap.Length <= 1)
+                nap = "0" + nap;
+            string datum = $"{random.Next(2000, 2026)}.{honap}.{nap}";
+            return datum;
         }
 
         private void btnSzobakezeles_Click(object sender, RoutedEventArgs e)
