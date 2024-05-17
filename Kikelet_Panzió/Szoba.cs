ddfca290def094ba12688da1892f035e21915c3c
JNapsugar <jancsurakn@kkszki.hu>
2024-05-17 09:17:14 +0200

    Initial commit

---
+++ Kikelet_Panzió/Szoba.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kikelet_Panzió
+{
+    internal class Szoba
+    {
+        int szobaszam { get; }
+        int ferohelyek { get; set; }
+        int ar { get; set; }
+
+        public Szoba(int szobaszam, int ferohelyek, int ar)
+        {
+            this.szobaszam = szobaszam;
+            this.ferohelyek = ferohelyek;
+            this.ar = ar;
+        }
+
+
+    }
+
+}
